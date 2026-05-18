5390cd2967a445f81f3983fdb365ab3c7184f7de
Opik1704 <dsgorch04@gmail.com>
2026-05-18 19:39:58 +0300

    Доработка обычной очереди из 3 лабы

--- src/TaskQueue.py
+++ src/TaskQueue.py
@@ -1,22 +1,63 @@
-from typing import Iterable, Iterator
+from typing import Iterable, Iterator, Callable, Generator
 from src.Task import Task
 
-class TaskQueue:
-    def __init__(self,tasks: Iterable[Task]):
-        """Инициализация"""
-        self.tasks = list(tasks) if not isinstance(tasks, list) else tasks
+class TaskQueueIterator:
+    """Настоящий(True) итератор для очереди задач """
+    def __init__(self, tasks):
+        """Инициализируем итератор"""
+        self._tasks = tasks
+        self._index = 0
+
     def __iter__(self) -> Iterator[Task]:
-        """Реализация Итерации"""
-        for task in self.tasks:
-            yield task
+        """возвращает итератор"""
+        return self
+
+    def __next__(self) -> Task:
+        """шаг итерации или StopIteration"""
+        if not hasattr(self._tasks, '__len__') or not hasattr(self._tasks, '__getitem__'):
+            raise StopIteration
+
+        if self._index >= len(self._tasks):
+            raise StopIteration
+        task = self._tasks[self._index]
+        self._index += 1
+        return task
+
+
+class TaskQueue:
+    """Очередь задач и ленивая фильтрация"""
+    def __init__(self,_tasks: Iterable[Task]):
+        """Инициализация"""
+        if _tasks is not None:
+            self.tasks = _tasks
+        else:
+            self.tasks = []
+
+    def add_task(self, task: Task) -> None:
+        """Добавление задачи в очередь """
+        if hasattr(self.tasks, 'append'):
+            self.tasks.append(task)
+        else:
+            raise TypeError("Нельзя добавить задачу в ленивый потоковый источник")
+
+    def __iter__(self) -> TaskQueueIterator:
+        """Итерация """
+        return TaskQueueIterator(self.tasks)
+
     def __len__(self) -> int:
         """Длина очереди"""
-        return sum(1 for task in self.tasks)
-    def filter_by_priority(self, priority: str) -> 'TaskQueue':
-        """Ленивый фильтр 1 создает новую очередь по приоритету """
-        filtered = (t for t in self.tasks if t.priority == priority)
-        return TaskQueue(filtered)
-    def filter_by_status(self, status: str) -> 'TaskQueue':
-        """Ленивый фильтр 2 по статусу"""
-        filtered = (t for t in self.tasks if t.status == status)
-        return TaskQueue(filtered)
+        if hasattr(self.tasks, '__len__'):
+            return len(self.tasks)
+        raise TypeError("Queue with lazy source doesn't support len()")
+
+    def filter_by_status(self, status: str) -> Generator[Task, None, None]:
+        """Ленивый фильтр по статусу"""
+        for task in self:
+            if task.status == status:
+                yield task
+
+    def filter_by_priority(self, priority: str) -> Generator[Task, None, None]:
+        """Ленивый фильтр по приоритету"""
+        for task in self:
+            if task.priority == priority:
+                yield task
